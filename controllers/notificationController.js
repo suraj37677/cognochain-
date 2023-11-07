e2c8ca0003a686b45000827e8db8a9b6ceebe7bc
suraj37677 <135296917+suraj37677@users.noreply.github.com>
2023-11-06 22:52:58 -0800

    Update notificationController.js

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,15 +1,15 @@
-// Import the Notification model and any other necessary dependencies.
+
 const Notification = require('../models/notificationModel');
 
 // Function to get user notifications
 exports.getNotifications = async (req, res) => {
   try {
-    const user = req.user; // Assuming user data is available in req.user.
+    const user = req.user; 
 
     // Retrieve user notifications from the database.
     const notifications = await Notification.find({ userId: user._id })
       .sort({ createdAt: -1 })
-      .limit(10); // Example: Get the latest 10 notifications.
+      .limit(10); 
 
     res.json({ notifications });
   } catch (error) {
