f8225ca8dad1379c0300040f6b1e6779cb380bf1
suraj37677 <135296917+suraj37677@users.noreply.github.com>
2023-11-06 22:49:03 -0800

    Update profileRoute.js

--- router/profileRoute.js
+++ router/profileRoute.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the User model and any other necessary dependencies.
-const User = require('../models/userModel');
 
-// Middleware for user authentication (you should implement this).
+const User = require('../models/userModel');
 const authenticateUser = (req, res, next) => {
   
   next();
